b589146c96082a311d928fa60c0b7487a6b4cd1a
Yuqing Fan <fan230@purdue.edu>
2022-02-15 19:05:03 -0500

    forward unit tb runs now case 11 overwrite by case 01

--- testbench/forward_unit_tb.sv
+++ testbench/forward_unit_tb.sv
@@ -27,8 +27,8 @@ module forward_unit_tb;
 `endif
 endmodule
 
-program test(control_unit_if fuif);
-
+program test(forward_unit_if fuif);
+  string test_num;
 task set_inputs;
     input ex_rs, ex_rt, mem_WrDest, wb_WrDest, mem_MemtoReg, mem_RegWr, wb_RegWr, dhit;
 begin
@@ -47,59 +47,61 @@ task read_output;
   input ex_A, ex_B;
 begin
   assert(ex_A == fuif.forwardA && ex_B == fuif.forwardB)
-    $display("Correct output for test case %0d", test_num);
-  else
-    $display("Incorrect output for test case %0d. Expected fwdA: %0d  , Actual fwdA: %0d", test_num, ex_A, fuif.forwardA);
-    $display("Incorrect output for test case %0d. Expected fwdB: %0d  , Actual fwdB: %0d", test_num, ex_B, fuif.forwardB);
-    end
+    $display("Correct output for test case %s", test_num);
+  else begin
+    $display("Incorrect output for test case %s. Expected fwdA: %0d  , Actual fwdA: %0d", test_num, ex_A, fuif.forwardA);
+    $display("Incorrect output for test case %s. Expected fwdB: %0d  , Actual fwdB: %0d", test_num, ex_B, fuif.forwardB);
+  end
+  end
 endtask
 
 parameter PERIOD = 10;
 initial begin
-    //set_inputs(ex_rs, ex_rt, mem_WrDest, wb_WrDest, mem_MemtoReg, mem_RegWr, wb_RegWr, dhit)
-    test_num = 0;
+    //set_inputs(ex_rs, ex_rt, mem_WrDest, wb_WrDest, 
+  //mem_MemtoReg, mem_RegWr, wb_RegWr, dhit)
+    test_num = "";
 
     //forward A rtype
-    test_num += 1;
+    test_num ="A 00";
     set_inputs('0,'0,'0,'0,0,0,0,0);
     #(PERIOD)
-    read_output(2'b00,'0);
+    read_output(2'b00,2'b00);
 
-    test_num += 1;
+    test_num ="A 11";
     set_inputs(5'd3,'0,5'd3,'0,1,1,0,1);
     #(PERIOD)
-    read_output(2'b11,'0);
+    read_output(2'b11,2'b00);
 
-    test_num += 1;
+    test_num ="A 10";
     set_inputs(5'd4,'0,5'd4,'0,0,1,0,0);
     #(PERIOD)
-    read_output(2'b10,'0);
+    read_output(2'b10,2'b00);
 
-    test_num += 1;
+    test_num ="A 01";
     set_inputs(5'd5,'0,'0,5'd5,0,0,1,0);
     #(PERIOD)
-    read_output(2'b01,'0);
+    read_output(2'b01,2'b00);
 
     //forward B itype
-    test_num += 1;
+    test_num ="B 00";
     set_inputs('0,'0,'0,'0,0,0,0,0);
     #(PERIOD)
-    read_output(2'b00,'0);
+    read_output(2'b00,2'b00);
 
-    test_num += 1;
+    test_num ="B 11";
     set_inputs('0,5'd3,5'd3,'0,1,1,0,1);
     #(PERIOD)
-    read_output(2'b11,'0);
+    read_output(2'b00,2'b11);
 
-    test_num += 1;
+    test_num ="B 10";
     set_inputs('0,5'd4,5'd4,'0,0,1,0,0);
     #(PERIOD)
-    read_output(2'b10,'0);
+    read_output(2'b00,2'b10);
 
-    test_num += 1;
+    test_num ="B 01";
     set_inputs('0,5'd5,'0,5'd5,0,0,1,0);
     #(PERIOD)
-    read_output(2'b01,'0);
+    read_output(2'b00,2'b01);
 
 
     $finish;
